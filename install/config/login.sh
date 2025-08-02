2dff2c215a3c1505116011808120598d1d798b5a
brunohcorreia <bhcorreia11@gmail.com>
2025-08-01 21:11:06 -0300

    algumas mudanças

--- install/config/login.sh
+++ install/config/login.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
 
-# Instalando o plymouth e o tema arch-logo
-sudo pacman -S --noconfirm plymouth plymouth-theme-arch-logo
+set -e
 
-# Define o tema padrão como arch-logo e já atualiza o initramfs
-sudo plymouth-set-default-theme -R arch-logo
+# Instala o gerenciador de login Ly
+sudo pacman -S --noconfirm --needed ly 
 
-# Atualiza o mkinitcpio.conf: insere 'plymouth' antes de 'systemd'
-sudo sed -i 's/^HOOKS=(\(.*\)systemd/\1plymouth systemd/' /etc/mkinitcpio.conf
-
-# Regenera o initramfs para todos os kernels
+# Regenera o initramfs (caso tenha feito alterações manuais no HOOKS antes)
 sudo mkinitcpio -P
 
-# Adiciona parâmetro 'splash' no GRUB
-sudo sed -i 's/GRUB_CMDLINE_LINUX="/GRUB_CMDLINE_LINUX="splash /' /etc/default/grub
-
-# Regenera o GRUB
+# Regenera o GRUB (caso queira garantir que está atualizado)
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 
-# Garante que o plymouth saia antes do login (ly) iniciar
-sudo mkdir -p /etc/systemd/system/ly.service.d
-echo -e "[Service]\nExecStartPre=/usr/bin/plymouth quit" | sudo tee /etc/systemd/system/ly.service.d/plymouth.conf
+# Habilita o Ly para iniciar com o sistema
+sudo systemctl disable DISPLAY_MANAGER
+sudo systemctl disable getty@tty2.service
+sudo systemctl enable ly.service
 
-echo -e "\n✅ Plymouth configurado com sucesso!\nSe quiser testar, reinicie com: sudo reboot"
+echo -e "\nLy configurado com sucesso!"
